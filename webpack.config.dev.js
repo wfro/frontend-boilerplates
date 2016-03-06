baef168230c3ecc08d16944ac9743faaa98d8121
Will Faurot <will@influxdb.com>
2016-03-05 21:12:03 -0800

    Add style loaders

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -16,10 +16,16 @@ module.exports = {
     new webpack.HotModuleReplacementPlugin(),
   ],
   module: {
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['babel'],
-      include: path.join(__dirname, 'src'),
-    }],
+    loaders: [
+      {
+        test: /\.js$/,
+        loaders: ['babel'],
+        include: path.join(__dirname, 'src'),
+      },
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader',
+      },
+    ],
   },
 };
